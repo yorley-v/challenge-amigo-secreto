5da2ef6e06fa613c7c939e09b7bc05ff3a77adcd
yorley-v <verayorley@gmail.com>
2025-02-07 11:07:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let nombreSecreto = Freyja;
+let nombreAmigo = prompt("Indica el nombre de tu amigo:");
+
+console.log(nombreAmigo);
+
+if (nombreAmigo == nombreSecreto) {
+    alert('Acertaste el nombre');
+}
