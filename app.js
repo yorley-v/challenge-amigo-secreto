bf938f15d3808489e43e5624fdae279ffbe88541
Yorley Vera <verayorley@gmail.com>
2025-02-18 14:36:18 -0300

    Actualiza 2

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+//Crear un array para almacenar la lista de amigos
 let amigos = []
+console.log = ('amigos');
 let listaAmigos = document.getElementById('listaAmigos');
-let 
 
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector (elemento);
-    elementoHTML.innerHTML = texto; 
-}
 
-function agregarAmigo() {
-    alert ('¡Hola, has hecho click en el botón!');
-}
 
-function resetList() {
-    alert ('¡Has iniciado una nueva partida!');
-}
+//function asignarTextoElemento(elemento, texto) {
+    //let elementoHTML = document.querySelector (elemento);
+    //elementoHTML.innerHTML = texto; 
+//}
 
-asignarTextoElemento('h1', 'Juego del Amigo Secreto');
-asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+//function agregarAmigo() {
+    //alert ('¡Hola, has hecho click en el botón!');
+//}
+
+//function resetList() {
+  //  alert ('¡Has iniciado una nueva partida!');
+//}
+
+//asignarTextoElemento('h1', 'Juego del Amigo Secreto');
+//asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
